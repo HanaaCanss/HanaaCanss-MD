2625326d4d458f717163feae5d7768c204ad442b
RIKUXD <94986583+RikuBotz@users.noreply.github.com>
2022-06-21 01:20:50 +0700

    Add files via upload

---
+++ plugins/main-donasi.js
@@ -0,0 +1,28 @@
+let fs = require('fs')
+let handler = async (m, { conn }) => {
+let teks = 'donasi'
+let dana = global.dana
+let pulsa = global.pulsa
+let gopay = global.gopay
+let numberowner = global.numberowner
+let anu = `Hai Kakk👋
+Kalian bisa mendukung hanaa agar bot ini tetap up to date dengan:
+╔═══〔 Donasi • Emoney 〕══➣
+╠🍭╠➥ Dana : ${dana}
+╠🍭╠➥ Pulsa : ${pulsa}
+╚════════════➣
+Berapapun donasi kalian akan sangat berarti 👍
+
+Arigatou!
+
+Contact person Owner:
+wa.me/${numberowner} (Owner)
+
+*donasi via follow ig juga boleh*`
+  conn.sendButtonImg(m.chat, fla + teks, anu, instagram, 'Back', '.help', m) 
+}
+handler.help = ['donasi', 'donate']
+handler.tags = ['xp', 'info']
+handler.command = /^(donasi|donate)$/i
+
+module.exports = handler
