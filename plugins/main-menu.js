966626fe48eab776e814e4ecc4c0a8006d89194a
RIKUXD <94986583+RikuBotz@users.noreply.github.com>
2022-06-21 02:07:26 +0700

    Add files via upload

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -23,20 +23,20 @@ let moment = require('moment-timezone')
 const defaultMenu = {
   before:`
 ╔═「 *${global.namebot}* 」
-╠📖╠➥ Hai, %name!
-╠📖╠➥ Tersisa *%limit Limit*
-╠📖╠➥ Role *%role*
-╠📖╠➥ Level *%level (%exp / %maxexp)*
-╠📖╠➥ [%xp4levelup]
-╠📖╠➥ %totalexp XP secara Total
+╠💻╠➥ Hai, %name!
+╠💻╠➥ Tersisa *%limit Limit*
+╠💻╠➥ Role *%role*
+╠💻╠➥ Level *%level (%exp / %maxexp)*
+╠💻╠➥ [%xp4levelup]
+╠💻╠➥ %totalexp XP secara Total
 ║ 
-╠📖╠➥ Tanggal: *%week %weton, %date*
-╠📖╠➥ Tanggal Islam: *%dateIslamic*
-╠📖╠➥ Waktu: *%time*
+╠💻╠➥ Tanggal: *%week %weton, %date*
+╠💻╠➥ Tanggal Islam: *%dateIslamic*
+╠💻╠➥ Waktu: *%time*
 ║
-╠📖╠➥ Uptime: *%uptime (%muptime)*
-╠📖╠➥ Database: %rtotalreg dari %totalreg
-╠📖╠➥ Memory Used : *${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB*
+╠💻╠➥ Uptime: *%uptime (%muptime)*
+╠💻╠➥ Database: %rtotalreg dari %totalreg
+╠💻╠➥ Memory Used : *${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB*
 ╚═════════════➣
 %readmore`.trimStart(), 
   header: '╔═ %category ',
@@ -344,91 +344,91 @@ const template = generateWAMessageFromContent(m.key.remoteJid, proto.Message.fro
                   "description": "Menampilkan Menu Exp",
                   "rowId": `${_p}? xp`
                 }, {
-                  "title": "║🐣║FUN  ╠©HANACANS",
+                  "title": "║🐣║FUN  ",
                   "description": "Menampilkan Menu Fun",
                   "rowId": `${_p}? fun`
                 }, {
-                  "title": "║🎮║GAME ╠©HANACANS",
+                  "title": "║🎮║GAME ",
                   "description": "Menampilkan Menu Game",
                   "rowId": `${_p}? game`
                 }, {
-                  "title": "║🧰║GITHUB ╠©HANACANS",
+                  "title": "║🧰║GITHUB ",
                   "description": "Menampilkan Menu Github",
                   "rowId": `${_p}? github`
                 }, {
-                  "title": "║🏢║GROUP ╠©HANACANS",
+                  "title": "║🏢║GROUP ",
                   "description": "Menampilkan Menu Group",
                   "rowId": `${_p}? group`
                 }, {
-                  "title": "║🖼║IMAGE ╠©HANACANS",
+                  "title": "║🖼║IMAGE ",
                   "description": "Menampilkan Menu Image",
                   "rowId": `${_p}? image`
                 }, {
-                  "title": "║📡║INTERNET ╠©HANACANS",
+                  "title": "║📡║INTERNET ",
                   "description": "Menampilkan Menu Internet",
                   "rowId": `${_p}? internet`
                 }, {
-                  "title": "║🕋║ISLAMIC ╠©HANACANS",
+                  "title": "║🕋║ISLAMIC ",
                   "description": "Menampilkan Menu Islam",
                   "rowId": `${_p}? islam`
                 }, {
-                  "title": "║🐚║KERANG ╠©HANACANS",
+                  "title": "║🐚║KERANG ",
                   "description": "Menampilkan Menu Kerang",
                   "rowId": `${_p}? kerang`
                 }, {
-                  "title": "║✒️║MAKER ╠©HANACANS",
+                  "title": "║✒️║MAKER ",
                   "description": "Menampilkan Menu Maker",
                   "rowId": `${_p}? maker`
                 }, {
-                  "title": "║👨‍💻║OWNER ╠©HANACANS",
+                  "title": "║👨‍💻║OWNER ",
                   "description": "Menampilkan Menu Owner",
                   "rowId": `${_p}? owner`
                 }, {
-                  "title": "║🎙️║PENGUBAH SUARA ╠©HANACANS",
+                  "title": "║🎙️║PENGUBAH SUARA ",
                   "description": "Menampilkan Menu Voice Changer",
                   "rowId": `${_p}? suara`
                 }, {
-                  "title": "║👑║PREMIUM ╠©HANACANS",
+                  "title": "║👑║PREMIUM ",
                   "description": "Menampilkan Menu Premium",
                   "rowId": `${_p}? premium`
                 }, {
-                  "title": "║📑║QUOTES ╠©HANACANS",
+                  "title": "║📑║QUOTES ",
                   "description": "Menampilkan Menu Quotes",
                   "rowId": `${_p}? quotes`
                 }, {
-                  "title": "║🗡️║RPG ╠©HANACANS",
+                  "title": "║🗡️║RPG ",
                   "description": "Menampilkan Menu Rpg",
                   "rowId": `${_p}? rpg`
                 }, {
-                  "title": "║🐾║STALKER ╠©HANACANS",
+                  "title": "║🐾║STALKER ",
                   "description": "Menampilkan Menu Stalker",
                   "rowId": `${_p}? stalk`
                 }, {
-                  "title": "║🔗║SHORT LINK ╠©HANACANS",
+                  "title": "║🔗║SHORT LINK ",
                   "description": "Menampilkan Menu Short Link",
                   "rowId": `${_p}? shortlink`
                 }, {
-                  "title": "║🛠║TOOLS MENU ╠©HANACANS",
+                  "title": "║🛠║TOOLS MENU ",
                   "description": "Menampilkan Menu Tools",
                   "rowId": `${_p}? tools`
                 }, {
-                  "title": "║📖║TEXT MAKER ╠©HANACANS",
+                  "title": "║📖║TEXT MAKER ",
                   "description": "Menampilkan Maker Text",
                   "rowId": `${_p}? text`
                 }, {
-                  "title": "║🧼║HENTAI ╠©HANACANS",
+                  "title": "║🧼║HENTAI",
                   "description": "Menampilkan Menu Hentai",
                   "rowId": `${_p}? nsfw`
                 }, {
-                  "title": "║🎲║RANDOM╠©HANACANS",
+                  "title": "║🎲║RANDOM",
                   "description": "Menampilkan Menu Random/Gabut",
                   "rowId": `${_p}? random`
                 }, {
-                  "title": "║📚║TEXT PRO ╠©HANACANS",
+                  "title": "║📚║TEXT PRO ",
                   "description": "Menampilkan Text Pro Menu",
                   "rowId": `${_p}? textpro`
                 }, {
-                  "title": "║💭║PHOTO OXY ╠©HANACANS",
+                  "title": "║💭║PHOTO OXY ",
                   "description": "Menampilkan Photo Oxy Menu",
                   "rowId": `${_p}? textpro`
                 }
