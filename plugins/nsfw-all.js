1787d922144db283d39d9bac562cf2850ac02334
HanaaCanss <94986583+HanaaCanss@users.noreply.github.com>
2022-06-22 11:17:12 +0700

    Add files via upload

--- plugins/nsfw-all.js
+++ plugins/nsfw-all.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, command }) => {
-let nyenye = `https://zenzapis.xyz/api/morensfw/${command}?apikey=${zenzkey}`
+let nyenye = `https://https://apikeyhanaa.herokuapp.com/api/nsfw/${command}?apikey=${hanaakey}`
     conn.sendButtonImg(m.chat, nyenye, 'Nih', wm2, 'Next', `.${command}`, m) 
 }
 handler.help = ['ahegao', 'ass', 'bdsm', 'blowjob', 'cum', 'ero', 'femdom', 'foot', 'gangbang', 'glasses', 'hentai', 'jahy', 'maid', 'manga', 'neko', 'orgy', 'panties', 'pussy','sfwneko', 'tentacles', 'thighs', 'yuri']
