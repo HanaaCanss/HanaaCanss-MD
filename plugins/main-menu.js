c3978562fc19be28fe6ce593b96df321b547be69
HanaaCanss <94986583+HanaaCanss@users.noreply.github.com>
2022-06-26 00:47:27 +0700

    Add files via upload

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -42,7 +42,7 @@ const defaultMenu = {
 ╚══════════════════⬣
 %readmore`.trimStart(), 
   header: '╔═ 【  %category  】 ',
-  body: '╠☰➵͜͡✏ %cmd %islimit %isPremium',
+  body: '╠☰➵͜͡❁ %cmd %islimit %isPremium',
   footer: '╚════\n', 
   after: ``,
 }
