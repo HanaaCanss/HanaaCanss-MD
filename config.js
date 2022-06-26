ce9a3b0cf8de60e45db59e2d48944d2ac52b2a96
HanaaCanss <94986583+HanaaCanss@users.noreply.github.com>
2022-06-27 00:55:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,17 +8,14 @@ global.nameowner = 'HanaaCanss'
 global.numberowner = '6281270275470'
 global.instagram = 'https://instagram.com/hyzer_stfu'
 global.github = 'PRIVATE'
-global.dana = '081267758318'
+global.dana = '083187631714'
 global.pulsa = '083187631714'
 
 //=============『 Info Bot 』=================//
 global.namebot = 'HanaaaBotzz'
 global.gc = 'https://chat.whatsapp.com/Jzd9DEVB5nODtNBk1VCNrV'
 global.web = 'universeworld.ml' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
-global.price1 = '╔══【 ıll *LIST* llı 】═֍\n\n╠➵͜͡⫹⫺ PERMANEN Rp 5000/AKUN\n╚═══════════֍\n\n╔══【 ıll *VIA* llı 】═֍
-╠➵͜͡⫹⫺ Dana : ${dana}
-╠➵͜͡⫹⫺ Pulsa : ${pulsa}
-╚══════════════════֍'
+global.price1 = '╔══【 ıll *LIST* llı 】═֍\n\nPERMANET Rp5000 PER AKUN\n\n╔══【 ıll *VIA* llı 】═֍\nDana : 083187631714\nPulsa : 083187631714''
 //=======『 Tampilan Dan Lainnya 』============//
 global.fotonya1 = 'https://telegra.ph/file/869c320fa92ee73e8e422.png' //ganti jadi foto bot mu
 global.fotonya2 = 'https://telegra.ph/file/951752b235e226cf94939.png' //ini juga ganti 
