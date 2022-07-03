9c80badefa9e3a4cced2b61ea7fb78b362534c64
HanaaCanss <94986583+HanaaCanss@users.noreply.github.com>
2022-07-03 17:01:04 +0700

    Add files via upload

--- plugins/main-sewa.js
+++ plugins/main-sewa.js
@@ -1,30 +1,31 @@
 let fs = require('fs')
 let handler = async (m, { conn }) => {
-let teks = 'SewaBot'
+let teks = 'sewa bot'
 let dana = global.dana
 let pulsa = global.pulsa
+let gopay = global.gopay
 let numberowner = global.numberowner
 let anu = `Hai Kakk👋
 Ini List Harga Sewanya Kakk
 ╔═══〔 Pembayaran Via 〕══➣
-╠🍭╠➥ PRIVATE CHAT = FREE
-╠🍭╠➥ 1 GRUP / BULAN = 5K
-╠🍭╠➥ 1 GRUP PERMANENT = 15K
+╠➥ PRIVATE CHAT = FREE
+╠➥ 1 GRUP / BULAN = 5K
+╠➥ 1 GRUP PERMANENT = 15K
 ╔═══〔 Pembayaran Via 〕══➣
-╠🍭╠➥ Dana : ${dana}
-╠🍭╠➥ Pulsa : ${pulsa}
+╠➥ Dana : ${dana}
+╠➥ Pulsa : ${pulsa}
 ╔═══〔 FITUR 〕══➣
-╠🍭╠➥ 24 JAM ONLINE
-╠🍭╠➥ FAST RESPOND
-╠🍭╠➥ VVIP BOT / PREMIUM
-╠🍭╠➥ FITUR 200+
+╠➥ 24 JAM ONLINE
+╠➥ FAST RESPOND
+╠➥ VVIP BOT / PREMIUM
+╠➥ FITUR 200+
 ╚═══════════════════➣
 
 Arigatou!
 
 Contact person Owner:
 wa.me/${numberowner} (Owner)`
-  conn.sendButtonImg(m.chat, fla + teks, anu, 'Back', '.help', m) 
+  conn.sendButtonImg(m.chat, fla + teks, anu, wm2, 'Back', '.help', m) 
 }
 handler.help = ['sewabot', 'sewa']
 handler.tags = ['xp', 'info']
