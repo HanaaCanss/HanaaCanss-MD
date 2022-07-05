55bee33ff7f1d88146823be9ade693f74f5da430
HanaaCanss <94986583+HanaaCanss@users.noreply.github.com>
2022-07-06 00:27:58 +0700

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -333,7 +333,7 @@ const template = generateWAMessageFromContent(m.key.remoteJid, proto.Message.fro
                 },{
                   "title": `⋮☰【💳】SEWA BOT`,
                   "description": "Sewa Untuk Grup Kakk",
-                  "rowId": `.sewazifa`
+                  "rowId": `.sewahana`
                 },{
                   "title": "⋮☰【❗】INFO BOT",
                   "description": "Menampilkan Menu Info",
