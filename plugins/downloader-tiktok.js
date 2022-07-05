9635991904c007d8f059c2fd7af0b09b09fc0c6b
HanaaCanss <94986583+HanaaCanss@users.noreply.github.com>
2022-07-05 19:32:53 +0700

    Add files via upload

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,12 +1,13 @@
 const hxz = require("hxz-api")
 let handler = async(m,{text, conn}) => {
 
+m.reply(wait)
 let p = await  hxz.ttdownloader(text)
 const { nowm, wm, audio } = p
-conn.sendFile(m.chat, nowm, null, 'Nih', m)
+conn.sendFile(m.chat, nowm, null, '*DONE✅*', m)
 }
 handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-handler.command = /^(tiktok)$/i
+handler.command = /^(tiktok|tiktoknowm)$/i
 
 module.exports = handler
