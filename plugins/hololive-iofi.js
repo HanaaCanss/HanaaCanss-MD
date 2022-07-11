d1137ff2aeaedb6c66bde489ba08191ff92156f2
HanaaCanss <94986583+HanaaCanss@users.noreply.github.com>
2022-07-12 01:28:59 +0700

    Update hololive-iofi.js

--- plugins/hololive-iofi.js
+++ plugins/hololive-iofi.js
@@ -16,4 +16,5 @@ global.iofi = [
 "https://i.postimg.cc/5NtQcGyb/Airani-Iofifteen-full-3040965.jpg",
 "https://i.postimg.cc/8zJFc6LY/Airani-Iofifteen-full-3068626.png",
 "https://i.postimg.cc/85DsZXx8/Airani-Iofifteen-full-3109885.jpg"
+
 ]
