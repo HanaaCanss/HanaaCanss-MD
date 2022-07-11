057ca1553e49661ab7d16642c73d5c4cea5bb710
HanaaCanss <94986583+HanaaCanss@users.noreply.github.com>
2022-07-12 01:14:24 +0700

    Update hololive-iofi.js

--- plugins/hololive-iofi.js
+++ plugins/hololive-iofi.js
@@ -12,8 +12,8 @@ module.exports = handler
 
 global.iofi = [
 "https://i.postimg.cc/GtL8mgjP/Airani-Iofifteen-full-3004292.png",
-"https://i.postimg.cc/59gQR1xs/Airani-Iofifteen-full-3027392.jpg",",
-"https://i.postimg.cc/5NtQcGyb/Airani-Iofifteen-full-3040965.jpg",",
+"https://i.postimg.cc/59gQR1xs/Airani-Iofifteen-full-3027392.jpg",
+"https://i.postimg.cc/5NtQcGyb/Airani-Iofifteen-full-3040965.jpg",
 "https://i.postimg.cc/8zJFc6LY/Airani-Iofifteen-full-3068626.png",
-"https://i.postimg.cc/85DsZXx8/Airani-Iofifteen-full-3109885.jpg",",
+"https://i.postimg.cc/85DsZXx8/Airani-Iofifteen-full-3109885.jpg"
 ]
