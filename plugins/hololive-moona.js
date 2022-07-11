94c4b703ea435fbc14d3ff51e43bbada62662ca9
HanaaCanss <94986583+HanaaCanss@users.noreply.github.com>
2022-07-12 01:15:01 +0700

    Update hololive-moona.js

--- plugins/hololive-moona.js
+++ plugins/hololive-moona.js
@@ -12,8 +12,8 @@ module.exports = handler
 
 global.moona = [
 "https://i.postimg.cc/BvSXgK2M/Kobo-Kanaeru-full-3615742.jpg",",
-"https://i.postimg.cc/FF8k41QH/Moona-Hoshinova-full-2909987.jpg",",
+"https://i.postimg.cc/FF8k41QH/Moona-Hoshinova-full-2909987.jpg",
 "https://i.postimg.cc/xTmJTY0y/Moona-Hoshinova-full-2988559.png",
-"https://i.postimg.cc/K8yK1VjX/Moona-Hoshinova-full-2993118.jpg",",
-"https://i.postimg.cc/ZnGWFsLd/Moona-Hoshinova-full-3034219.jpg",",
+"https://i.postimg.cc/K8yK1VjX/Moona-Hoshinova-full-2993118.jpg",
+"https://i.postimg.cc/ZnGWFsLd/Moona-Hoshinova-full-3034219.jpg"
 ]
